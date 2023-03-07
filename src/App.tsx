28ae1218645e941bf45ab80618a202a4f8ed23ca
Muhammad Ans <anstig3r@gmail.com>
2023-03-07 05:27:15 +0500

    Refactored Default Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// import fonts
+// import './fonts/Helvetica-Light.ttf';
+// import './fonts/Helvetica.ttf';
+// import './fonts/Helvetica-Bold.ttf';
+// import './fonts/Helvetica-Oblique.ttf';
+// import './fonts/Helvetica-BoldOblique.ttf';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Hello World!
     </div>
   );
 }
